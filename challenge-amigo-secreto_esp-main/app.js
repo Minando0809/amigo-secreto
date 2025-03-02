3dad07045af286f84ce5ece9c36ed5f27830624c
Mario Ocampo <134954945+Minando0809@users.noreply.github.com>
2025-03-02 17:03:47 -0500

    Actualizacion de JS code

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,21 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos =[];
+const  amigos =[];
 
 function agregarAmigo(){
     const inputAmigo = document.getElementById('amigo');
     const nombreAmigo = inputAmigo.value.trim();
 
 
-   if(nombre === ""){
-      alert("por favor, inserte un nombre.");
+   if(nombreAmigo === ""){
+      alert("por favor, inserte los nombres.");
       return;
 
     }
 
    if (amigos.includes(nombreAmigo)){
-         alert(`El nombre ${nombreAmigo} ya esta en la lista`);
-        return;
+       alert(`El nombre "${nombreAmigo}" ya esta en la lista`);
+      return;
     }
 
 
@@ -31,3 +31,34 @@ function agregarAmigo(){
 }
 
 
+function actualizarLista(){
+  const listaAmigos = document.getElementById('listaAmigos');
+
+  listaAmigos.innerHTML = "";
+
+  amigos.forEach(amigo => { // Use forEach for better readability
+    const li = document.createElement('li');
+    li.textContent = amigo; // Set the text content
+    listaAmigos.appendChild(li); // Append the new list item
+  });
+    
+}
+
+
+function sortearAmigo() {
+
+  if (amigos.length === 0) {
+    alert("no hay nombres para realizar el sorteo. agregar nombres a la lista.");
+    return;
+  }
+
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+  const amigoSorteado = amigos[indiceAleatorio];
+
+  const resultado = document.getElementById('resultado');
+
+  resultado.innerHTML = `Amigo sorteado: <strong>${amigoSorteado}</strong>`;
+
+
+}
